9a0ff3f03b17cc8d7edc45a3b4f117316fcb4872
Kang Jeong Hyun <kjh3081801@naver.com>
2026-05-14 02:00:40 +0900

    local setting change

--- pipeline/model_download.py
+++ pipeline/model_download.py
@@ -2,8 +2,8 @@ import os
 from optimum.intel import OVSentenceTransformer
 
 save_path = "./models/bge-small-ov2"
-model_id = "BAAI/bge-small-en-v1.5"
-# model_id = "./models/bge-small-local"
+#model_id = "BAAI/bge-small-en-v1.5"
+model_id = "./models/bge-small-local"
 os.makedirs("./models", exist_ok=True)
 
 model = OVSentenceTransformer.from_pretrained(model_id, export=True)
